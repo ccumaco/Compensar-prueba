382524eb31515bd5a5d45c3cdeade1d2737330dd
ccumaco <carloscumaco5@gmail.com>
2021-09-23 17:02:22 -0500

    ultimos ajustes funciones js

--- scripts/form.js
+++ scripts/form.js
@@ -1,6 +1,9 @@
 let informations = document.getElementsByClassName('information')
 let listStep = document.getElementsByClassName('step')
 let dialog = document.getElementsByClassName('dialog')[0]
+function scrollInformation() {
+    window.scroll(0, 530)
+}
 function simulate(step) {
     console.log(step);
     if (step == 5) {
