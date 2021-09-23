e26a7f7f5eff21f0e44bdb4cf561721e0ad15b97
ccumaco <carloscumaco5@gmail.com>
2021-09-23 16:37:46 -0500

    initial commit

---
+++ scripts/form.js
@@ -0,0 +1,20 @@
+let informations = document.getElementsByClassName('information')
+let listStep = document.getElementsByClassName('step')
+let dialog = document.getElementsByClassName('dialog')[0]
+function simulate(step) {
+    console.log(step);
+    if (step == 5) {
+        dialog.setAttribute('style', 'display: none')
+    }
+    if (step == 4) {
+        dialog.setAttribute('style', 'display: flex')
+    } else {
+        if (step == 2 || step == 3) {
+            console.log('entrooo');
+            listStep[step - 2].className = 'step'
+            listStep[step - 1].className += ' active'
+        }
+        informations[step - 1].className = 'information'
+        informations[step].className += ' active'
+    }
+}
